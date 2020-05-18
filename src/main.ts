f8f2ebd8c365e0f520a58dce7847b381c245f688
Acidiney Dias <acidineydias@gmail.com>
2020-05-18 01:14:00 +0100

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  await app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
