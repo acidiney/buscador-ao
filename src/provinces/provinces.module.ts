e9b615abae2548565367e05824990e0287827cb6
Jorge Dacosta <jorgeMSDC@mail.com>
2020-07-25 10:59:46 +0100

    improvement: register the Province Schema

--- src/provinces/provinces.module.ts
+++ src/provinces/provinces.module.ts
@@ -1,4 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Province, ProvinceSchema } from './schemas/province.schema';
 
-@Module({})
+@Module({
+    imports: [
+        MongooseModule.forFeature([
+            {
+                name: Province.name,
+                schema: ProvinceSchema 
+            }
+        ])
+    ]
+})
 export class ProvincesModule {}
