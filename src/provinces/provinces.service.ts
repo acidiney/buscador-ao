4db41de67cfbbf89418435b653e01e560aff1235
Jorge Dacosta <jorgeMSDC@mail.com>
2020-07-31 15:25:36 +0100

    feat: add fn to find all provinces

--- src/provinces/provinces.service.ts
+++ src/provinces/provinces.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Province } from './schemas/province.schema';
+import { Model } from 'mongoose';
+import { CreateProvinceDto } from './dtos/create-province.dto';
 
 @Injectable()
-export class ProvincesService {}
+export class ProvincesService {
+    constructor(@InjectModel(Province.name) private provinceModel: Model<Province>) {}
+
+    async findAll(): Promise<Province[]> {
+        return await this.provinceModel
+        
+        .find()
+        .exec();
+    }
+}
