e8d2434442be11fc8ebf4e694d59bf1ce9f27309
Jorge Dacosta <jorgeMSDC@mail.com>
2020-08-16 08:43:46 +0100

    fix: remove the insert-many route

--- src/provinces/provinces.controller.ts
+++ src/provinces/provinces.controller.ts
@@ -6,11 +6,6 @@ import { CreateProvinceDto } from './dtos/create-province.dto';
 export class ProvincesController {
     constructor(private readonly provincesService: ProvincesService) {}
 
-    @Post('insert-many')
-    async insertMany(@Body() createProvinceDto: CreateProvinceDto []) {
-        return await this.provincesService.insertMany(createProvinceDto);
-    }
-
     @Get()
     async findAll() {
         return await this.provincesService.findAll();
