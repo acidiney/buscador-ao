c0d9b1c06f9a40d4f30e4f80847da5d8634ee531
Jorge Dacosta <jorgeMSDC@mail.com>
2020-08-23 09:46:28 +0100

    improvement: export provinces service & Schema

--- src/provinces/provinces.module.ts
+++ src/provinces/provinces.module.ts
@@ -14,6 +14,7 @@ import { ProvincesController } from './provinces.controller';
         ])
     ],
     providers: [ProvincesService],
-    controllers: [ProvincesController]
+    controllers: [ProvincesController],
+    exports: [ProvincesService, MongooseModule]
 })
 export class ProvincesModule {}
