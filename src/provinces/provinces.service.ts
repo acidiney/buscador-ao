414b52a6315aacea77dce9b428530c0d5f70f240
Jorge Dacosta <jorgeMSDC@mail.com>
2020-08-23 09:48:44 +0100

    feat(province/service): add create fn
    Also changed the way to find province by ID;

--- src/provinces/provinces.service.ts
+++ src/provinces/provinces.service.ts
@@ -1,12 +1,23 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Province } from './schemas/province.schema';
 import { Model } from 'mongoose';
 import { CreateProvinceDto } from './dtos/create-province.dto';
+import { Province } from './schemas/province.schema';
 
 @Injectable()
 export class ProvincesService {
     constructor(@InjectModel(Province.name) private provinceModel: Model<Province>) {}
+
+    async create(province: CreateProvinceDto): Promise<CreateProvinceDto> {
+        const { name } = province;
+        const exists = await this.provinceModel.findOne({ name });
+        
+        if (!!exists) {
+            throw new BadRequestException('Esta província já existe no banco de dados.');
+        }
+
+        return await this.provinceModel.create(province);
+    }
 
     async findAll(): Promise<Province[]> {
         return await this.provinceModel
@@ -15,8 +26,8 @@ export class ProvincesService {
         .exec();
     }
 
-    async findById(_id: string): Promise<CreateProvinceDto> {
-        const province = await this.provinceModel.findOne({ _id });
+    async findById(id: string): Promise<CreateProvinceDto> {
+        const province = await this.provinceModel.findById(id);
 
         if (!province) {
             console.log(province);
