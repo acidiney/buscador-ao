464b4f3e6a2080d266fb978553adc55f751c2a97
Acidiney Dias <acidineydias@gmail.com>
2020-08-30 01:35:46 +0100

    ci(heroku): load port from heroku

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,6 @@ async function bootstrap() {
   });
   const app = await NestFactory.create(AppModule);
   await app.enableCors();
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
