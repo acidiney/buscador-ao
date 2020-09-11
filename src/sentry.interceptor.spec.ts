13a5cc0a447f1fc94190e2ba64164d1cd3874669
Acidiney Dias <acidineydias@gmail.com>
2020-09-11 20:10:34 +0100

    ci(devops): change sentry to newrelic

--- src/sentry.interceptor.spec.ts
+++
@@ -1,7 +0,0 @@
-import { SentryInterceptor } from './sentry.interceptor';
-
-describe('SentryInterceptor', () => {
-  it('should be defined', () => {
-    expect(new SentryInterceptor()).toBeDefined();
-  });
-});
